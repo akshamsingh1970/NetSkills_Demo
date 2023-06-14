28253e1a3a51d21e3a64f2e055e1b7cc87bd9032
Samiul Haque <samiul.haque@ZL-BLR-MAC597.local>
2023-06-14 16:45:33 +0530

    firsttt

---
+++ sam/reversepant.java
@@ -0,0 +1,30 @@
+public class reversepant{
+    public static void main(String [] args){
+        
+        //public static void main(String[] args) {
+
+		for(int i=1; i<=5; i++)
+
+		{
+
+		    for(int j=1; j<=9; j++)
+
+		    {
+
+		        if(j>=1+i && j<=9-i)
+
+		        System.out.print(" ");
+
+		        else
+
+		        System.out.print("*");
+
+		    }
+
+		    System.out.print("\n");
+
+		}
+
+	}
+    }
+    
