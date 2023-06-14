b9ac260f92b5e37fb61eb9810b481786b2671c2f
G O Amrutha Praveen <amrutha.praveen@ZL-BLR-MAC551.local>
2023-06-14 15:55:36 +0530

    Added solution to 14/06 first question

---
+++ Solutions/test.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class class1 {
+
+    public int num1;
+    public int num2;
+
+    public class1 (int a, int b) {
+        this.num1 = a;
+        this.num2 = b;
+    }
+    public int sum(int a, int b) {
+        return (num1+num2);
+    }
+}
+
+class class2 extends class1 {
+    public class2 (int a, int b) {
+        super(a,b);
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a, b;
+        System.out.println("Enter 2 numbers to be added : ");
+
+        a = s.nextInt();
+        b = s.nextInt();
+
+        class2 obj = new class2 (a,b);
+        System.out.println(obj.sum(a,b));
+
+    }
+}
