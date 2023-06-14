bc8ca67b387bd27e2784cf54bcb2bc630ca73b13
Nampally Pranav <nampally.pranav@ZL-BLR-MAC580.local>
2023-06-14 16:30:47 +0530

    Q5 answer final

--- jundhanAccount.java
+++ jundhanAccount.java
@@ -4,4 +4,24 @@ public class jundhanAccount extends SBIBankAccount{
         this.balance=0;
         System.out.println("Jundhan Account opened with balance= "+this.balance);
     }
+    @Override
+    public void deposit(int amount){
+        this.balance+=amount;
+        System.out.println();
+        System.out.println(amount+" amount is deposited in Jundhan Account");
+        System.out.println("Current Balance in Jundhan Account: "+this.balance);
+    }
+    @Override
+    public void withdraw(int amount){
+        System.out.println();
+        if(amount<=this.balance) {
+            this.balance -= amount;
+            System.out.println(amount + " amount is withdrawn from the Jundhan Account");
+            System.out.println("Current Balance in Jundhan Account: "+this.balance);
+        }
+        else{
+            System.out.println("Can't withdraw, Balance is Insufficient");
+            System.out.println("Current Balance in Jundhan Account: "+this.balance);
+        }
+    }
 }
--- premiumAccount.java
+++ premiumAccount.java
@@ -6,16 +6,23 @@ public class premiumAccount extends SBIBankAccount{
         System.out.println("Premium Account opened with balance= "+this.balance);
     }
     @Override
+    public void deposit(int amount){
+        this.balance+=amount;
+        System.out.println();
+        System.out.println(amount+" amount is deposited in Premium Account");
+        System.out.println("Current Balance in Premium Account: "+this.balance);
+    }
+    @Override
     public void withdraw(int amount){
         System.out.println();
         if(this.balance>=amount+5000) {
             this.balance -= amount;
-            System.out.println(amount + " amount is withdrawn");
-            System.out.println("Current Balance: "+this.balance);
+            System.out.println(amount + " amount is withdrawn from the Premium Account");
+            System.out.println("Current Balance in Premium Account: "+this.balance);
         }
         else{
-            System.out.println("Can't withdraw, Balance is Insufficient");
-            System.out.println("Current Balance: "+this.balance);
+            System.out.println("Can't withdraw, Balance is Insufficient in the Premium Account");
+            System.out.println("Current Balance in Premium Account: "+this.balance);
         }
     }
 }
