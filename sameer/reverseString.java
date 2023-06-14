20296d8a7064cbff2458fd0dd31c275e68ee5594
Mohammad Sameer <mohammad.sameer@ZL-BLR-MAC574.local>
2023-06-14 15:27:18 +0530

    Added file to reverse string

---
+++ sameer/reverseString.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class reverseString {
+    public static void main(String[] args) {
+        Scanner inputObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter string to reverse:");
+
+        String str = inputObj.nextLine();  // Read user input
+        
+        // Reversing a string
+        System.out.println("Original string: "+ str);
+        System.out.println("Reversed String: "+ revStr(str));
+    }
+
+    public static String revStr(String str) {
+        String revStr = "";
+        for(int i = str.length() - 1; i >= 0; i--) {
+            revStr += str.charAt(i);
+        }
+        
+        return revStr;
+    }
+}
