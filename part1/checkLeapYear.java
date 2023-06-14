281f85debaa33dca63be76fdb71c73e2b94e1301
Harsh.kumar <harsh.kumar1@ZL-BLR-MAC556.local>
2023-06-15 00:17:44 +0530

    first commit

---
+++ part1/checkLeapYear.java
@@ -0,0 +1,20 @@
+public class checkleapyear {
+    public static void main(String[] args) {
+
+        int year = 1990;
+
+        // Implementing our algorithm.
+        if (year % 400 == 0) {
+            System.out.println(year + " is a leap year.");
+        }
+        else if (year % 100 == 0) {
+            System.out.println(year + " is not a leap year.");
+        }
+        else if (year % 4 == 0) {
+            System.out.println(year + " is a leap year.");
+        }
+        else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
