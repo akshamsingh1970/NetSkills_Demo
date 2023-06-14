ad056fc7fa939068f73129d07884bd8788656e59
Aayush Meshram <aayush.meshram@ZL-BLR-MAC529.local>
2023-06-15 00:01:42 +0530

    assignment from aayush-m

---
+++ aayush-m/p10.java
@@ -0,0 +1,15 @@
+public class p10    {
+    public static void main(String[] args) {
+        for(int i = 4; i >= 0; i--)   {
+            for(int j = 0; j <= i; j++) 
+                System.out.print("* ");
+            for(int j = i; j < 4; j++)  
+                System.out.print("  ");
+            for(int j = i; j < 4; j++)  
+                System.out.print(" ");
+            for(int j = 0; j <= i; j++) 
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
