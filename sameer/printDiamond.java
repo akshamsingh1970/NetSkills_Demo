7d42f4c1f72030e4bbd43312eed6c541d6a0d225
Mohammad Sameer <mohammad.sameer@ZL-BLR-MAC574.local>
2023-06-14 15:44:19 +0530

    Added code to print diamond

---
+++ sameer/printDiamond.java
@@ -0,0 +1,25 @@
+public class printDiamond {
+    public static void main(String[] args) {
+        int n = 12;
+        int totalPlaces = n + (n - 1);
+        for(int i = 1; i <= n; i++) {
+            int spaces = (totalPlaces - i - (i - 1)) / 2;
+            for(int k = 0; k < spaces; k++)
+                System.out.print(" "); 
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
+        for(int i = n-1; i >= 1; i--) {
+            int spaces = (totalPlaces - i - (i - 1)) / 2;
+            for(int k = 0; k < spaces; k++)
+                System.out.print(" "); 
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
