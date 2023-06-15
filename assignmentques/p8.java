a7a3773e51495906c5dde30c0a7baa4daa5df12a
Ritik Kumar <ritik.kumar@ZL-BLR-MAC594.local>
2023-06-15 10:32:01 +0530

    ritik added 1 to 12 ques

---
+++ assignmentques/p8.java
@@ -0,0 +1,6 @@
+public class p8 {
+    public static void main(String[] args) {
+        int year = 1990;
+        System.out.println((year%4==0)?year+" is a leap year":year+" is not a leap year");
+    }
+}
